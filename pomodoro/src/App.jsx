d5b60cf940d5c2499f59e3f955d62c94504baea8
Your Name <shubhamverma6351@gmail.com>
2023-04-27 01:26:35 +0530

    initial commit

---
+++ pomodoro/src/App.jsx
@@ -0,0 +1,16 @@
+import CounterPage from "./components/CounterPage"
+import Github from "./components/Github"
+function App() {
+
+  return (
+    <>
+      <div >
+        <Github />
+        <CounterPage />
+    </div>
+      
+    </>
+  )
+}
+
+export default App
